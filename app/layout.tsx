b41ea333e9fca8cc9ed8ac956e8aaa4540f76e4d
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-01-21 03:09:08 +0530

    initialize Next.js project with Convex integration and Tailwind CSS setup

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ConvexClientProvider } from "./convex-client-provider";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "TodoRex",
+  description: 
+  "TodoRex seamlessly organizes your tasks and predicts what's next using AI.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+       <ConvexClientProvider> {children} </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Tasks from "@/components/todorex/tasks";
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-8 row-start-2 items-center justify-between p-24">
+        <h1>TodoRex</h1>
+        <Button>Click me</Button>
+
+        <Tasks />
+    </main>
+  );
+}
