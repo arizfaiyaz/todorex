ed3b3587f520a628aed285035c32686717f66daa
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-01-22 00:20:20 +0530

    add authentication actions and NextAuth integration with Google provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "./convex-client-provider";
+import { ConvexClientProvider } from "./providers";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,15 +1,13 @@
 
+import Tasks from "@/components/todorex/tasks";
 import { Button } from "@/components/ui/button";
-import { signInAction } from "@/actions/auth-actions";
+
 
 export default function Home() {
   return (
       <main className="flex flex-col gap-8 row-start-2 items-center justify-between p-24">
         <h1>TodoRex</h1>
-
-        <form action={signInAction}>
-        <Button>Log in</Button>
-        </form>
+        <Tasks />
     </main>
   );
 }
